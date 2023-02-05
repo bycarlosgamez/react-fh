13d62fbfd5f10ffb17c6501b88700866ddd878f3
Carlos Gámez <hardpx@gmail.com>
2023-02-04 22:32:10 -0600

    added substract and add functions

--- counter-app/src/Counter.jsx
+++ counter-app/src/Counter.jsx
@@ -1,10 +1,29 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Counter = ({ value }) => {
+  const [number, setNumber] = useState(value);
+
+  function handleAdd() {
+    setNumber((prevNum) => prevNum + 1);
+  }
+
+  function substract() {
+    setNumber((prevNum) => prevNum - 1);
+  }
+
   return (
     <>
       <h1>Counter App</h1>
-      <h2>{value}</h2>
+      <div className='counter'>
+        <button className='button' onClick={substract}>
+          -
+        </button>
+        <h2> {number} </h2>
+        <button className='button' onClick={handleAdd}>
+          +
+        </button>
+      </div>
     </>
   );
 };
