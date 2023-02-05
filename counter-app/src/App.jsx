b1071af4facc95d0dc1145ce4a85fc7b1701df1b
Carlos Gámez <hardpx@gmail.com>
2023-02-04 19:26:42 -0600

    added installed proptypes and added to app component

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,9 +1,17 @@
-const App = () => {
+import PropTypes from 'prop-types';
+
+const App = ({ title, subtitle }) => {
   return (
     <div className='container'>
-      <h1>Hola Mundo!!!</h1>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
     </div>
   );
+};
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
 };
 
 export default App;
