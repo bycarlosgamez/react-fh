5f1fe7db2ed0463f9ea7357ee697f1c3042c3d63
Carlos Gámez <hardpx@gmail.com>
2023-02-08 20:49:20 -0600

    added on addd category function

--- gif-expert-app/src/App.jsx
+++ gif-expert-app/src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+
 function App() {
+  const [categories, setCategories] = useState(['Hades', 'Zelda']);
+
+  const onAddCategory = () => {
+    setCategories((prevCategories) => {
+      return [...prevCategories, 'Hollow Knight'];
+    });
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <button onClick={onAddCategory}>Add</button>
+
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 }
