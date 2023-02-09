7e75efefa79d010dc23c9fb3531c3814fbac77ca
Carlos Gámez <hardpx@gmail.com>
2023-02-08 18:18:04 -0600

    added basic styles and styles css

--- gif-expert-app/src/App.jsx
+++ gif-expert-app/src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className='container'>
+    <>
       <h1>GifExpertApp</h1>
-    </div>
+    </>
   );
 }
 
