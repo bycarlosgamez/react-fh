efa31a309c21ed4f54dd78d7a4e5f796185615ef
Carlos Gámez <hardpx@gmail.com>
2023-02-15 11:09:53 -0600

    added title, url and alt test

--- gif-expert-app/tests/components/GifCard.test.jsx
+++ gif-expert-app/tests/components/GifCard.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import GifCard from '../../src/components/GifCard';
 
 describe('<GifCard /> Tests', () => {
@@ -7,5 +7,18 @@ describe('<GifCard /> Tests', () => {
   test('must match with snapshot', () => {
     const { container } = render(<GifCard url={url} title={title} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test('should show img with url and correct alt', () => {
+    render(<GifCard url={url} title={title} />);
+    // screen.debug();
+    const { src, alt } = screen.getByRole('img');
+    expect(src).toBe(url + '/');
+    expect(alt).toBe(title);
+  });
+
+  test('should show title', () => {
+    render(<GifCard url={url} title={title} />);
+    expect(screen.getByText(title)).toBeTruthy();
   });
 });
