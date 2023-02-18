3fa960b9b2af49cd91550c5bacfba1805fcbaf58
Carlos Gámez <hardpx@gmail.com>
2023-02-17 22:33:17 -0600

    update

--- gif-expert-app/tests/components/GifGrid.test.jsx
+++ gif-expert-app/tests/components/GifGrid.test.jsx
@@ -1,10 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import GifGrid from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('<GidGri /> test', () => {
   const category = 'test';
 
   test('should initially show loading', () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
     render(<GifGrid category={category} />);
 
     // screen.debug();
